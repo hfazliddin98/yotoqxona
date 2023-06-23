6761f280d064cc35e26163d7ed581d73f1056fa4
hfazliddin98 <hatamovfazliddin505@gmail.com>
2023-06-23 10:33:26 +0500

    home

--- asosiy/urls.py
+++ asosiy/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from users.views import home
 
 urlpatterns = [
     path('haker/', admin.site.urls),
+    path('', home, name='home'),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):    
+    data = 'Saytda texnik ishlar olib borilmaoqda'
+    contex = {
+        'data':data,
+    }
+    return render(request, 'home.html', contex)
