b205775546624e63bb9c6f3bc8d273fa52dab880
hfazliddin98 <hatamovfazliddin505@gmail.com>
2023-07-25 16:02:37 +0500

    server kode

--- server_code.py
+++ server_code.py
@@ -1,3 +1,6 @@
 # server ip 192.168.0.24
 # login yotoqxona
 # parol Yotoqxona2023
+
+# nastroyka
+# https://www.digitalocean.com/community/tools/nginx?domains.0.php.php=false&domains.0.python.python=true&domains.0.python.djangoRules=true&domains.0.routing.root=false
